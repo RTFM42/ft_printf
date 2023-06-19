03f147170bb0db027c70c5405c02df31fdd3915d
yushsato <yushsato@student.42tokyo.jp>
2023-06-19 18:29:09 +0900

    update

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/12 20:23:39 by yushsato          #+#    #+#              #
-#    Updated: 2023/06/16 21:12:09 by yushsato         ###   ########.fr        #
+#    Updated: 2023/06/19 18:00:56 by yushsato         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	= libftprintf.a
 CC		= gcc
 FLAGS	= -Wall -Wextra -Werror
-SRCS	= main.c
+SRCS	= ft_printf.c
 OBJS	= $(SRCS:.c=.o)
 
 all: $(NAME)
--- ft_printf.c
+++ ft_printf.c
@@ -3,21 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
+/*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/16 21:12:17 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/16 23:16:07 by yushsato         ###   ########.fr       */
+/*   Created: 2023/06/19 17:46:20 by yushsato          #+#    #+#             */
+/*   Updated: 2023/06/19 18:01:32 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
+#include "ft_printf.h"
 
-void	ft_printf(const char *fmt, ...)
+void ft_printf(char *fmt, ...)
 {
 	va_list	ap;
+	int		d;
+	char	c
+	char	*s;
 
 	va_start(ap, fmt);
-	printf("%s", fmt);
+	while (*fmt)
+		if (ft_strncmp(fmt, "%c", 2) == 0)
+		{
+			fmt++;
+
+		}
+//		switch(*fmt++) {
+//			case 's':
+//			s = va_arg(ap, char *);
+//			printf("string %s\n", s);
+//			break;
+//		case 'd':		       /* int */
+//			d = va_arg(ap, int);
+//			printf("int %d\n", d);
+//			break;
+//		case 'c':		       /* char */
+//			/* Note: char is promoted to int. */
+//			c = va_arg(ap, int);
+//			printf("char %c\n", c);
+//			break;
+//		}
 	va_end(ap);
 }
