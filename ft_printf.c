f8c6071a714ae1151ec8d4fe0a55f9bc1083473d
yushsato <yushsato@student.42tokyo.jp>
2023-06-19 11:20:48 +0900

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,18 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 21:12:17 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/16 21:16:38 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/16 23:16:07 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_printf(const char fmt*, ...)
-{
+#include <stdio.h>
+#include <stdarg.h>
 
+void	ft_printf(const char *fmt, ...)
+{
+	va_list	ap;
+
+	va_start(ap, fmt);
+	printf("%s", fmt);
+	va_end(ap);
 }
