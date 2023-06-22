13fc277cb64b87c63e6d21174f131229e05f9534
yushsato <yushsato@student.42tokyo.jp>
2023-06-22 22:01:07 +0900

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:46:20 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/22 17:47:53 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/22 21:59:01 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,13 @@ int	ft_printf(char *fmt, ...)
 
 int	main(void)
 {
-	ft_printf("c\t:%c\ns\t:%s\np\t:%p\n", 't', "test", "ppp");
-	ft_printf("d\t:%d\ni\t:%i\nu\t:%u\n", -1, -10, 10);
-	ft_printf("x\t:%x\nX\t:%X\n", 12345, 12345);
+	int	len;
+
+	len = ft_printf("c\t:%c\ns\t:%s\np\t:%p\n", 't', "test", "ppp");
+	printf("%d\n", len);
+	len = ft_printf("d\t:%d\ni\t:%i\nu\t:%u\n", -1, -10, 10);
+	printf("%d\n", len);
+	len = ft_printf("x\t:%x\nX\t:%X\n", 12345, 12345);
+	printf("%d\n", len);
 	return (0);
 }
