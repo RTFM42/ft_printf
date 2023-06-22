9a28af43ec437eeb370eb55858c2d7d98f0361b0
yushsato <yushsato@student.42tokyo.jp>
2023-06-22 21:22:02 +0900

    update

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 21:18:44 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/22 17:48:16 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/22 21:22:00 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,5 @@
 # include <unistd.h>
 # include "libft/libft.h"
 # include "extra/extra.h"
-
-// int	ft_printf(const char *fmt, ...);
 
 #endif
