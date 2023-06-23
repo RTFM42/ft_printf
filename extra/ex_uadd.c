dddcd35b3fb6aaec6cb00e6c44e03cf67d97e5f1
es-yukun <yukun@team.anylinks.jp>
2023-06-23 23:51:47 +0900

    update

--- extra/ex_uadd.c
+++ extra/ex_uadd.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 01:21:34 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/23 23:40:24 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/23 23:51:45 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ int	ex_uadd(int i1, char i2)
 		i1++;
 		i2--;
 	}
-	if (i1 <= INT_MAX && i2 == 0)
-		return (i1);
 	if (i1 == INT_MAX && i2 > 0)
 		return (-1);
+	return (i1);
 }
