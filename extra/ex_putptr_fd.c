e352609ca705f620ae5d939e2411f34f077adaa4
es-yukun <yukun@team.anylinks.jp>
2023-06-24 18:22:55 +0900

    update

--- extra/ex_putptr_fd.c
+++ extra/ex_putptr_fd.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 23:41:00 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/23 23:45:50 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/24 18:21:55 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	ex_putptr_fd(void *ptr, int fd)
 	int	len;
 
 	write(fd, "0x", 2);
-	len = ex_uadd(len, ex_putunbr_base_fd((size_t)ptr, "0123456789abcdef", fd));
+	len = ex_putunbr_base_fd((size_t)ptr, "0123456789abcdef", fd);
 	return (len + 2);
 }
