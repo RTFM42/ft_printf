fa481072e8b4fdc925650ba9bdfddccb77f3073e
es-yukun <yukun@team.anylinks.jp>
2023-06-24 23:12:15 +0900

    update

--- extra/ex_uadd.c
+++ extra/ex_uadd.c
@@ -6,14 +6,14 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 01:21:34 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/24 18:22:23 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/24 23:11:12 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "extra.h"
 #include <limits.h>
 
-int	ex_uadd(int i1, char i2)
+int	ex_uadd(int i1, int i2)
 {
 	if (i1 < 0 || i2 < 0)
 		return (-1);
